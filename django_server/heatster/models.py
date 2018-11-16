409cd436ddccbce90e13ec2554d32739c83ceff1
Nozzy <msahlg17@student.aau.dk>
2018-11-16 01:03:33 +0100

    Added initial models for the database

--- django_server/heatster/models.py
+++ django_server/heatster/models.py
@@ -1,7 +1,45 @@
 from django.db import models
 
-# Model for the Room
 
-# Model for Valve
+# Model for the Weekdays
+class Weekdays(models.Model):
+    name = models.CharField(max_length=255)
 
-# Model for Schedule
+
+# Model for the Rooms
+class Rooms(models.Model):
+    name = models.CharField(max_length=255)
+
+
+# Model for Valves
+class Valves(models.Model):
+    room_id = models.IntegerField()
+    current_temp = models.FloatField()
+
+
+# Model for Schedules
+class Schedules(models.Model):
+    room_id = models.IntegerField()
+    weekday_id = models.IntegerField()
+    temperature = models.CharField(max_length=255)
+
+
+# Model for Records
+class Records(models.Model):
+    room_id = models.IntegerField()
+    temperature = models.FloatField()
+    time = models.DateTimeField()
+
+
+# Model for Vacations
+class Vacations(models.Model):
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+
+# Model for Settings
+class Settings(models.Model):
+    vacation_temp = models.IntegerField()
+    sleep_temp = models.IntegerField()
+    sleep_start = models.TimeField()
+    sleep_end = models.TimeField()
