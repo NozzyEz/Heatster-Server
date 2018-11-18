a52df67b00977e356a30d80670bd8e47a8f4fabc
Nozzy <msahlg17@student.aau.dk>
2018-11-18 10:11:20 +0100

    went through the models, and named them correctly, as well as all the database tables

--- django_server/heatster/admin.py
+++ django_server/heatster/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Weekday, Room, Valve, Schedule, Record, Vacation, Setting
 
-# Register your models here.
+admin.site.register(Weekday)
+admin.site.register(Room)
+admin.site.register(Valve)
+admin.site.register(Schedule)
+admin.site.register(Record)
+admin.site.register(Vacation)
+admin.site.register(Setting)
--- django_server/heatster/models.py
+++ django_server/heatster/models.py
@@ -1,44 +1,66 @@
 from django.db import models
-
+""" This file is for the models that are used to create and manipulate tables in our database,
+this gives us a much better interface for doing queries """
 
 # Model for the Weekdays
-class Weekdays(models.Model):
+class Weekday(models.Model):
+    class Meta:
+        db_table = 'heatster_weekdays'
+
     name = models.CharField(max_length=255)
 
 
 # Model for the Rooms
-class Rooms(models.Model):
+class Room(models.Model):
+    class Meta:
+        db_table = 'heatster_rooms'
+
     name = models.CharField(max_length=255)
 
 
 # Model for Valves
-class Valves(models.Model):
+class Valve(models.Model):
+    class Meta:
+        db_table = 'heatster_valves'
+
     room_id = models.IntegerField()
     current_temp = models.FloatField()
 
 
 # Model for Schedules
-class Schedules(models.Model):
+class Schedule(models.Model):
+    class Meta:
+        db_table = 'heatster_schedules'
+
     room_id = models.IntegerField()
     weekday_id = models.IntegerField()
     temperature = models.CharField(max_length=255)
 
 
 # Model for Records
-class Records(models.Model):
+class Record(models.Model):
+    class Meta:
+        db_table = 'heatster_records'
+
     room_id = models.IntegerField()
     temperature = models.FloatField()
     time = models.DateTimeField()
 
 
 # Model for Vacations
-class Vacations(models.Model):
+class Vacation(models.Model):
+    class Meta:
+        db_table = 'heatster_vacations'
+    
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
 
 
 # Model for Settings
-class Settings(models.Model):
+class Setting(models.Model):
+    class Meta:
+        db_table = 'heatster_settings'
+
     vacation_temp = models.IntegerField()
     sleep_temp = models.IntegerField()
     sleep_start = models.TimeField()
