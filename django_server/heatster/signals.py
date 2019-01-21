4d56d6ce90d5e1f52f10550e378d4fcdbe6dc121
Nozzy <msahlg17@student.aau.dk>
2019-01-21 15:17:28 +0100

    server now checks the schedules table for a set_temp value which it then stores for the appropiate room

--- django_server/heatster/signals.py
+++ django_server/heatster/signals.py
@@ -1,7 +1,7 @@
 import datetime as dt
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from .models import Valve, Room
+from .models import Valve, Room, Schedule
 
 @receiver(post_save, sender=Valve)
 def room_temp_handler(sender, instance, **kwargs):
@@ -31,17 +31,27 @@ def room_temp_handler(sender, instance, **kwargs):
 
 @receiver(post_save, sender=Valve)
 def set_room_temp(sender, instance, **kwargs):
+    ''' Also listens for updates to the valve, but this function goes and looks for
+    the desired temperature the valve should be set to according to the schedule, and 
+    stores that in the room the valve is in '''
     # Get the current time and weekday from the server
     current_hour = dt.datetime.now().hour
-    current_day = dt.datetime.today().weekday
-    print("The hour is {} on the {}st day of the week".format(current_hour, current_day))
-    # Get the current room id
-
+    current_day = dt.datetime.today().weekday() + 1
+    
     # Find the corresponding schedule for the room and weekday
+    current_schedule = Schedule.object.filter(room_id=instance.room.id, weekday_id=current_day)
 
     # Extract the temperature string and store it as a list
-
+    string_object = current_schedule.values_list('temperature', flat=True)
+    temp_string = string_object[0]
+    temperatures = temp_string.split(";")
+        
     # Use the current hour of the time to get the proper index of the list where the temperature is
-
+    room_set_temp = temperatures[current_hour]
+    
     # Write the temp to the room's set_temp
+    for room in Room.object.filter(id=instance.room.id):
+        room.set_temp = room_set_temp
+        room.save()
+
 
